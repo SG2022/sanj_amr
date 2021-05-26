bc1409b55bf824af925a89f4d59e9bf53b415969
Raushan Kumar <raushank@invento.tech>
2021-05-26 17:59:01 +0530

    added code with pwm use

--- catkin_ws/src/amr/pwm_subs.py
+++ catkin_ws/src/amr/pwm_subs.py
@@ -4,6 +4,8 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist 
 import RPi.GPIO as GPIO          
 from time import sleep
+
+# defined pins
 in1 = 24
 in2 = 23
 in3 = 22
@@ -12,7 +14,7 @@ en1 = 25
 en2 =4
 temp1=1
 
-#setting up gpio pins
+# setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
@@ -25,80 +27,93 @@ GPIO.output(in2,GPIO.LOW)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 
-#setting up pwm pins
+# setting up pwm 
 p1=GPIO.PWM(en1,1000)
 p2=GPIO.PWM(en2,1000)
 
-# initial setup for duty
-p1.start(25)
-p2.start(25)
+move_pwm = 60
+turn_pwm = 45
+
+p1.start(move_pwm)
+p2.start(move_pwm)
+
+max_pwm = 90
+print("All the pin setup done")
 
 def callback(data):
-    lin = data.linear.x
-    ang = data.angular.z
+    global max_pwm, turn_pwm move_pwm
+    x = data.linear.x
+    z = data.angular.z
 
-    # converting into duty cycle from the teleop message by mulitplying it to 10
-    duty = lin * 10
+    move_pwm = x * 50
+    turn_pwm = x * 30
 
-    # filter if duty cycle exceeds the limit
-    if duty > 80:
-        duty = 80
+    if(move_pwm > max_pwm):
+        move_pwm = max_pwm 
+
+    if(turn_pwm > max_pwm):
+        turn_pwm = max_pwm 
     
-    if lin>0:
-       forward(duty)
-    elif lin<0: 
-       backward(duty)
-    if(ang<0):
-       right(duty)
-    else: 
-       left(duty)  
-    if (lin==0 and ang==0):
+    if (x>0):
+       forward()
+    elif (x<0): 
+       backward()
+    elif(z<0):
+       right()
+    elif(z>0):
+       left()
+    elif (x==0 and z==0):
        stop()
 
-def forward(duty):
-    print("forward with duty :{} ".format(duty))
-    p1.ChangeDutyCycle(duty)
-    p2.ChangeDutyCycle(duty)
-    GPIO.output(in1,GPIO.HIGH)
+def forward():
+    global max_pwm, turn_pwm move_pwm
+    print("forward")
+    p1.ChangeDutyCycle(move_pwm)
+    p2.ChangeDutyCycle(move_pwm)
+    GPIO.output(in1,GPIO.HIGH) # anti
     GPIO.output(in2,GPIO.LOW)
-    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in3,GPIO.HIGH) # clock
     GPIO.output(in4,GPIO.LOW)
 
-def backward(duty):
-    print("backward with duty :{} ".format(duty))
-    p1.ChangeDutyCycle(duty)
-    p2.ChangeDutyCycle(duty)
+def backward():
+    global max_pwm, turn_pwm move_pwm
+    print("backward")
+    p1.ChangeDutyCycle(move_pwm)
+    p2.ChangeDutyCycle(move_pwm)
     GPIO.output(in1,GPIO.LOW)
     GPIO.output(in2,GPIO.HIGH)
     GPIO.output(in3,GPIO.LOW)
     GPIO.output(in4,GPIO.HIGH)
 
-def right(duty):
-    print("right with duty :{} ".format(duty))
-    p1.ChangeDutyCycle(duty)
-    p2.ChangeDutyCycle(duty)
+def right():
+    global max_pwm, turn_pwm move_pwm
+    print("right turn")
+    p1.ChangeDutyCycle(turn_pwm)
+    p2.ChangeDutyCycle(turn_pwm)
     GPIO.output(in1,GPIO.HIGH)
     GPIO.output(in2,GPIO.LOW)
-    GPIO.output(in3,GPIO.HIGH)
-    GPIO.output(in4,GPIO.LOW)
- 
-def left(duty):
-    print("left with duty :{} ".format(duty))
-    p1.ChangeDutyCycle(duty)
-    p2.ChangeDutyCycle(duty)
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+
+def left():
+    global max_pwm, turn_pwm move_pwm
+    print("left  turn")
+    p1.ChangeDutyCycle(turn_pwm)
+    p2.ChangeDutyCycle(turn_pwm)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
     GPIO.output(in3,GPIO.HIGH)
     GPIO.output(in4,GPIO.LOW)
 
 def stop():
+    global max_pwm, turn_pwm move_pwm
+    print("stop")  
     p1.ChangeDutyCycle(0)
     p2.ChangeDutyCycle(0)
     GPIO.output(in1,GPIO.LOW)
     GPIO.output(in2,GPIO.LOW)
     GPIO.output(in3,GPIO.LOW)
     GPIO.output(in4,GPIO.LOW)
-    print("stop with duty :{} ".format(duty))
 
 def listener():
     rospy.init_node('cmdvel_subscriber', anonymous=True)
